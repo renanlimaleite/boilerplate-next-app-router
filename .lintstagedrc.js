b5c43b4ae62fe29700755e515cdafa8336c8d97e
renanlimaleite <renan.lima.softwares@gmail.com>
2023-09-10 10:02:45 -0300

    init tests 2

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,11 @@
 module.exports = {
   // Lint & Prettify TS and JS Files
-  '*.{js,jsx,ts,tsx}': (filenames) => [
-    `prettier --write ${filenames.join(' ')}`,
-    `npm run lint --fix . ${filenames.join(' --file')}`,
-    `npm test -- --findRelatedTests ${filenames.join(' ')}`
-  ]
+  '*.{js,jsx,ts,tsx}': (filenames) => {
+    console.log('Filenames:', filenames)
+    return [
+      `prettier --write ${filenames.join(' ')}`,
+      `npm run lint --fix . ${filenames.join(' --file')}`,
+      `npm test -- --findRelatedTests ${filenames.join(' ')}`
+    ]
+  }
 }
